06dd3e72b2b54d26acdf879232a549675ea08f44
mathRyan889 <matheusryan889@gmail.com>
2026-01-13 13:16:31 -0300

    Static update

--- app/settings.py
+++ app/settings.py
@@ -117,10 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'MeuSite' / 'static',
-]
+STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
