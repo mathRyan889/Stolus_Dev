4a7598c04c3a815fb7f58c486700347a74ade926
mathRyan889 <matheusryan889@gmail.com>
2026-01-13 12:57:21 -0300

    Update django settings

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-n%b_6drvl5d4szp44qsbpg+kr!7919)g5x+5x5$%2sqvb$&zn@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ryan20.pythonanywhere.com']
 
 
 # Application definition
@@ -122,3 +123,4 @@ STATICFILES_DIRS = [
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
