f7362e7a4f9b828f163ccd2c9d6e51d173bd3c40
mathRyan889 <matheusryan889@gmail.com>
2026-01-24 17:13:49 -0300

    Painel de vendas

--- MeuSite/admin.py
+++ MeuSite/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Lead
+from .models import Lead, Sales_Dashboard
 
+@admin.register(Lead)
 class LeadAdmin(admin.ModelAdmin):
     list_display = ('name', 'email', 'phone', 'created_at', 'updated_at')
     search_fields = ('name', 'email', 'phone')
     list_filter = ('created_at', 'updated_at')
 
-admin.site.register(Lead, LeadAdmin)
+
+
+@admin.register(Sales_Dashboard)
+class SalesDashboardAdmin(admin.ModelAdmin):
+    list_display = ('user', 'lead', 'service_interested', 'budget', 'status', 'created_at', 'updated_at')
+    search_fields = ('user__username', 'lead__name', 'service_interested', 'status')
+    list_filter = ('status', 'created_at', 'updated_at')
+
--- MeuSite/models.py
+++ MeuSite/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 class Lead(models.Model):
     name = models.CharField(max_length=100, verbose_name="Nome")
     email = models.EmailField(verbose_name="Email")
@@ -13,3 +13,18 @@ class Lead(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Sales_Dashboard(models.Model):
+    user = models.ForeignKey(User, on_delete=models.PROTECT, verbose_name="Usuário")
+    lead = models.ForeignKey(Lead, on_delete=models.PROTECT, verbose_name="Lead")
+    service_interested = models.CharField(max_length=100, verbose_name="Serviço de Interesse")
+    budget = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Orçamento")
+    status = models.BooleanField(default=False, verbose_name="Fechado")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Criado em")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Atualizado em")
+    description = models.TextField(verbose_name="Descrição")
+
+
+    class Meta:
+        verbose_name = "Dashboard de Vendas"
+        verbose_name_plural = "Dashboards de Vendas"
